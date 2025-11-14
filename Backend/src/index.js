b15f8710c19483ca8f3a4e94ea82e26d7abd50db
Ian Ale <ianalebom@gmail.com>
2025-11-14 20:31:51 +0700

    db

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,12 +1,10 @@
 import express from "express";
 
 const app = express();
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.json({ message: "Hello World!" });
 });
 
-
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(3000, () => console.log("Server running on port 3000"));
